8f1ed3702c3dac6a0e25da7f6660fe7f1836cb64
cjw0011 <chenjingwen008@gmail.com>
2025-08-04 22:37:57 +0800

    Update index.jsx
    ADD use jwt auth

--- src/pages/Budget/index.jsx
+++ src/pages/Budget/index.jsx
@@ -1,10 +1,13 @@
 import { PageContainer } from '@ant-design/pro-components';
 
 export default () => {
+  const token = localStorage.getItem('accessToken');
+  const iframeUrl = `https://budget.mylabcdd.top:30046/?token=${token}`;
+
   return (
     <PageContainer title={false}>
       <iframe
-        src="https://budget.mylabcdd.top:30046/"
+        src={iframeUrl} 
         style={{ width: '100%', height: '80vh', border: 'none' }}
         title="Budget"
       />
